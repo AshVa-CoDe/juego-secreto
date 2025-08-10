11a60247c10bed2b212dfb99e9ff105af84b06b5
Ashly Cuevas Almonte <105179004+AshVa-CoDe@users.noreply.github.com>
2025-08-10 09:18:38 -0400

    Version final juego numero secreto

---
+++ app.js
@@ -0,0 +1,67 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHtml = document.querySelector(elemento);
+  elementoHtml.innerHTML = texto;
+}
+
+function verificarIntento() {
+  let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+
+  if (numeroDeUsuario === numeroSecreto) {
+    asignarTextoElemento(
+      "p",
+      `Acertaste el número en ${intentos} ${intentos === 1 ? "vez" : "veces"}`
+    );
+    document.getElementById("reiniciar").removeAttribute("disabled");
+  } else {
+    if (numeroDeUsuario > numeroSecreto) {
+      asignarTextoElemento("p", "El número es menor");
+    } else {
+      asignarTextoElemento("p", "El número es mayor");
+    }
+    intentos++;
+    limpiarCaja();
+  }
+}
+
+function limpiarCaja() {
+  document.querySelector("#valorUsuario").value = "";
+}
+
+function generarNumeroSecreto() {
+  // Si ya sorteamos todos los números
+  if (listaNumerosSorteados.length === numeroMaximo) {
+    asignarTextoElemento("p", "Ya se sortearon todos los números posibles");
+    document.getElementById("reiniciar").removeAttribute("disabled"); // habilitar botón
+    return null; // No genera más números
+  }
+
+  let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+
+  if (listaNumerosSorteados.includes(numeroGenerado)) {
+    return generarNumeroSecreto(); // Buscar otro número
+  } else {
+    listaNumerosSorteados.push(numeroGenerado);
+    return numeroGenerado;
+  }
+}
+
+function condicionesIniciales() {
+  asignarTextoElemento("h1", "Juego del número secreto");
+  asignarTextoElemento("p", `Indica un número del 1 al ${numeroMaximo}`);
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
+
+function reiniciarJuego() {
+  limpiarCaja();
+  listaNumerosSorteados = []; // Vacíar la lista para nuevo ciclo
+  condicionesIniciales();
+  document.querySelector("#reiniciar").setAttribute("disabled", "true");
+}
+
+condicionesIniciales();
